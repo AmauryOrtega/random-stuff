66d68634b267c2f47cd0e0c293e5b733f377ae23
Amaury Ortega <amauryocortega@gmail.com>
2018-03-24 09:20:26 -0500

    Detectando IP para pi-hole
    Con el comando up.sh es posible hacer `source up.sh` y asi tener la
    variable de SHELL llamada `IP` que es usada en docker-compose-armhf.yml,
    faltan hacer pruenas en x86

---
+++ Docker/pi-hole/up.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ $(uname -m) = "x86_64" ]]
+then
+	# To Do, reconocer si esta conectado por cable o wifi
+	echo "Architectura X86"
+	$INTERFAZ="wlp3s0"
+	if [[ ! -z $1 ]]
+	then
+		$INTERFAZ=$1
+	fi
+	IP="$(ip addr show $INTERFAZ | awk '/inet / {print$2}' | cut -d/ -f1)"
+elif [[ $(uname -m) = "armv7l" ]]
+then
+	echo "Architectura armhf"
+	IP="$(ip addr show eth0 | awk '/inet / {print$2}' | cut -d/ -f1)"
+fi
+echo "IP: "$IP
