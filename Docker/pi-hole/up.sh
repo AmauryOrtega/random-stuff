13f26a94d97b98fdb0a40b687e6969660126ca5d
Amaury Ortega <amauryocortega@gmail.com>
2018-03-24 09:23:40 -0500

    Fix en up.sh, mala asignacion
    Corrigiendo la asignacion de la variable INTERFAZ

--- Docker/pi-hole/up.sh
+++ Docker/pi-hole/up.sh
@@ -3,10 +3,10 @@ if [[ $(uname -m) = "x86_64" ]]
 then
 	# To Do, reconocer si esta conectado por cable o wifi
 	echo "Architectura X86"
-	$INTERFAZ="wlp3s0"
+	INTERFAZ="wlp3s0"
 	if [[ ! -z $1 ]]
 	then
-		$INTERFAZ=$1
+		INTERFAZ=$1
 	fi
 	IP="$(ip addr show $INTERFAZ | awk '/inet / {print$2}' | cut -d/ -f1)"
 elif [[ $(uname -m) = "armv7l" ]]
