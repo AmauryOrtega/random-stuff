6860b1b1fa5061775114b581d033748bdc10d7f2
Amaury Ortega <amauryocortega@gmail.com>
2018-08-06 10:51:28 -0500

    Funciona

--- Docker/speedtest-cron/worker/src/worker.py
+++ Docker/speedtest-cron/worker/src/worker.py
@@ -1,14 +1,16 @@
 import time, os, speedtest, json
 
 SLEEP_TIME = os.environ.get('TIME')
+print('Usando {}s entre pruebas'.format(SLEEP_TIME))
 
 while True:
-    time.sleep(int(SLEEP_TIME))
     s = speedtest.Speedtest()
     #s.get_servers(servers)
     s.get_best_server()
     s.download()
     s.upload()
-    print(s.results.json())
+    print(s.results.server)
+    print(s.results.json(pretty=True))
     #with open('data.json', 'w') as output:
     #    json.dump(s.results.dict(), output, indent=4)
+    time.sleep(int(SLEEP_TIME))
